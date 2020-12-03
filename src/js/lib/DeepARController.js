1b0913bd17e6082f904343c458c44d71899d1663
a_hancho-desktop <atsuhan220@gmail.com>
2020-12-03 20:58:24 +0900

    feat : add moripearl effects and page

--- src/js/lib/DeepARController.js
+++ src/js/lib/DeepARController.js
@@ -59,6 +59,77 @@ export default class DeepARController {
   }
 
   switchEffect(effectIndex) {
-    this.deepAR.switchEffect(0, 'slot', DEEPAR_EFFECTS[effectIndex]);
+    this.deepAR.switchEffect(0, 'slot', this.option.effectPaths[effectIndex]);
   }
 }
+
+// deeparReference
+/*
+// Events
+deepAR.onCameraPermissionAsked = () => {
+  console.log('camera permission asked');
+};
+
+deepAR.onCameraPermissionGranted = () => {
+  console.log('camera permission granted');
+};
+
+deepAR.onCameraPermissionDenied = () => {
+  console.log('camera permission denied');
+};
+
+deepAR.onScreenshotTaken = (photo) => {
+  console.log('screenshot taken');
+};
+
+deepAR.onImageVisibilityChanged = (visible) => {
+  console.log('image visible', visible);
+};
+
+deepAR.onFaceVisibilityChanged = (visible) => {
+  console.log('face visible', visible);
+};
+*/
+
+/*
+// EcternalVideo
+const startExternalVideo = () => {
+  // create video element
+  let video = document.createElement('video');
+  video.muted = true;
+  video.loop = true;
+  video.controls = true;
+  video.setAttribute('playsinline', 'playsinline');
+  video.style.width = '100%';
+  video.style.height = '100%';
+
+  // put it somewhere in the DOM
+  let videoContainer = document.createElement('div');
+  videoContainer.appendChild(video);
+  videoContainer.style.width = '1px';
+  videoContainer.style.height = '1px';
+  videoContainer.style.position = 'absolute';
+  videoContainer.style.top = '0px';
+  videoContainer.style.left = '0px';
+  videoContainer.style['z-index'] = '-1';
+  document.body.appendChild(videoContainer);
+
+  navigator.mediaDevices
+    .getUserMedia({ video: true })
+    .then(stream => {
+      try {
+        video.srcObject = stream;
+      } catch (error) {
+        video.src = URL.createObjectURL(stream);
+      }
+
+      setTimeout(() => {
+        video.play();
+      }, 50);
+    })
+    .catch(error => {});
+
+  // tell the DeepAR SDK about our new video element
+  deepAR.setVideoElement(video, true);
+};
+*/
